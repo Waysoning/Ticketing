da4d523a07e71d2b154aefcfe37d794f02a59a38
wayson <wayson1225@outlook.com>
2022-03-07 12:53:49 -0800

    bad request error

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
 import { User } from '../models/user';
 import { RequestValidationError } from '../errors/request-validation-error';
+import { BadRequestError } from '../errors/bad-request-error';
 // import { DatabaseConnectionError } from '../errors/database-connection-error';
 
 const router = express.Router();
@@ -36,8 +37,9 @@ router.post(
     const existingUSer = await User.findOne({ email });
 
     if (existingUSer) {
-      console.log('Email in use');
-      return res.send({});
+      // console.log('Email in use');
+      // return res.send({});
+      throw new BadRequestError('Email in use');
     }
 
     const user = User.build({ email, password });
