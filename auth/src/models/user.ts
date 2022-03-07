48cfc9e4ca2d63e2fccac6634f92f8320394868d
wayson <wayson1225@outlook.com>
2022-03-07 13:18:53 -0800

    mongoose pre-save hooks

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import { Password } from '../services/password';
 
 // An interface that describes the properties
 // that are required to create a new User
@@ -31,6 +32,17 @@ const userSchema = new mongoose.Schema({
     required: true,
   },
 });
+
+// hash the password
+// userSchema.pre('save', async (done) => { // no good will override
+userSchema.pre('save', async function (done) {
+  if (this.isModified('password')) {
+    const hashed = await Password.toHash(this.get('password'));
+    this.set('password', hashed);
+  }
+  done();
+});
+
 userSchema.statics.build = (attrs: UserAttrs) => {
   return new User(attrs);
 };
