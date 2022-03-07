51ba750454a317692a0aab3dda1fe3c28026d41e
wayson <wayson1225@outlook.com>
2022-03-06 21:58:56 -0800

    common field and method of errors

--- auth/src/middlewares/error-handlers.ts
+++ auth/src/middlewares/error-handlers.ts
@@ -12,15 +12,17 @@ export const errorHandler = (
 
   if (err instanceof RequestValidationError) {
     console.log('handling this error as a request validation error');
-    const formattedErrors = err.errors.map((error) => {
-      return { message: error.msg, field: error.param };
-    });
-    return res.status(400).send({ errors: formattedErrors });
+    // const formattedErrors = err.errors.map((error) => {
+    //   return { message: error.msg, field: error.param };
+    // });
+    // return res.status(400).send({ errors: formattedErrors });
+    return res.status(err.statusCode).send({ errors: err.serializeErrors() });
   }
 
   if (err instanceof DatabaseConnectionError) {
-    console.log('handling this error as a db connection error');
-    return res.status(500).send({ errors: [{ message: err.reason }] });
+    // console.log('handling this error as a db connection error');
+    // return res.status(500).send({ errors: [{ message: err.reason }] });
+    return res.status(err.statusCode).send({ errors: err.serializeErrors() });
   }
 
   res.status(400).send({
