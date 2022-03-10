927e2586de8be91a0f2c03da433f6f37cbcc5e5f
wayson <wayson1225@outlook.com>
2022-03-09 22:29:51 -0800

    pass through cookies

--- client/pages/index.js
+++ client/pages/index.js
@@ -11,16 +11,14 @@ const LandingPage = ({ currentUser }) => {
   return <h1>Landing Page</h1>;
 };
 
-LandingPage.getInitialProps = async () => {
+LandingPage.getInitialProps = async ({ req }) => {
   if (typeof window === 'undefined') {
     // we are on the server
     // requests should make to ........
     const { data } = await axios.get(
       'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
       {
-        headers: {
-          Host: 'ticketing.dev',
-        },
+        headers: req.headers,
       }
     );
     // alternative way
