b8b929a961db426c535df8b9a44ef4dac8fd04ed
wayson <wayson1225@outlook.com>
2022-03-09 22:21:46 -0800

    namespace & request from broswer or server

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,40 @@
-const Landing = () => {
+import axios from 'axios';
+
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
+
+  // work on broswer
+  // axios.get('/api/users/currentuser').catch((err) => {
+  //   console.log(err.message);
+  // });
+
   return <h1>Landing Page</h1>;
 };
 
-export default Landing;
+LandingPage.getInitialProps = async () => {
+  if (typeof window === 'undefined') {
+    // we are on the server
+    // requests should make to ........
+    const { data } = await axios.get(
+      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+      {
+        headers: {
+          Host: 'ticketing.dev',
+        },
+      }
+    );
+    // alternative way
+    return data;
+  } else {
+    // we are on the broswer
+    // requests can be made with a base url of ''
+    const response = await axios.get('/api/users/currentuser');
+    // { currentUser : {} || null}
+    return response.data;
+  }
+
+  // work on client when navigate from one page to another
+  // mostly work on server and should deal with domain issue
+};
+
+export default LandingPage;
