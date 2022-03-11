96ce26a58601fe2a5c01c9697f0aeb2b6d67f71d
wayson <wayson1225@outlook.com>
2022-03-11 00:39:54 -0800

    test router

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -4,6 +4,8 @@ import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
 import { errorHandler, NotFoundError } from '@wayson-ticketing/common';
 
+import { createTicketRouter } from './routes/new';
+
 const app = express();
 app.set('trust proxy', true);
 app.use(json());
@@ -13,6 +15,8 @@ app.use(
     secure: process.env.NODE_ENV !== 'test',
   })
 );
+
+app.use(createTicketRouter);
 
 app.get('*', async (req, res) => {
   // next(new NotFoundError());
