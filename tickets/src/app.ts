36c55aa7f019e1c3288fc85f690ae2e91b741cf0
wayson <wayson1225@outlook.com>
2022-03-13 13:28:42 -0700

    test show routes

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -9,6 +9,7 @@ import {
 } from '@wayson-ticketing/common';
 
 import { createTicketRouter } from './routes/new';
+import { showTicketRouter } from './routes/show';
 
 const app = express();
 app.set('trust proxy', true);
@@ -22,6 +23,7 @@ app.use(
 app.use(currentUser);
 
 app.use(createTicketRouter);
+app.use(showTicketRouter);
 
 app.get('*', async (req, res) => {
   // next(new NotFoundError());
