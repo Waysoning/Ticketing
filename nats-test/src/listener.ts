09dfb6113cc5882ba9fba58e5e9bf7444d90288c
wayson <wayson1225@outlook.com>
2022-03-14 22:29:58 -0700

    extend the listener

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -15,25 +15,27 @@ stan.on('connect', () => {
     process.exit();
   });
 
-  const options = stan
-    .subscriptionOptions()
-    .setManualAckMode(true)
-    .setDeliverAllAvailable()
-    .setDurableName('accounting-service');
-  const subscription = stan.subscribe(
-    'ticket:created',
-    'listenerQueueGroup',
-    options
-  );
+  new TicketCreatedListener(stan).listen();
 
-  subscription.on('message', (msg: Message) => {
-    console.log('Message received');
-    const data = msg.getData();
-    if (typeof data == 'string') {
-      console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
-    }
-    msg.ack();
-  });
+  // const options = stan
+  //   .subscriptionOptions()
+  //   .setManualAckMode(true)
+  //   .setDeliverAllAvailable()
+  //   .setDurableName('accounting-service');
+  // const subscription = stan.subscribe(
+  //   'ticket:created',
+  //   'listenerQueueGroup',
+  //   options
+  // );
+
+  // subscription.on('message', (msg: Message) => {
+  //   console.log('Message received');
+  //   const data = msg.getData();
+  //   if (typeof data == 'string') {
+  //     console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
+  //   }
+  //   msg.ack();
+  // });
 });
 
 process.on('SIGINT', () => stan.close());
@@ -80,5 +82,15 @@ abstract class Listener {
     return typeof data === 'string'
       ? JSON.parse(data)
       : JSON.parse(data.toString('utf-8'));
+  }
+}
+
+class TicketCreatedListener extends Listener {
+  subject = 'ticket:created';
+  queueGroupName = 'payments-service';
+
+  onMessage(data: any, msg: nats.Message): void {
+    console.log('Event data!', data);
+    msg.ack();
   }
 }
