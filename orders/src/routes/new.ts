c492eb8353b1efe9de1eab1a8a0c3ae89de39a8a
wayson <wayson1225@outlook.com>
2022-03-17 21:40:19 -0700

    complete new router

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -13,6 +13,8 @@ import { Order } from '../models/order';
 
 const router = express.Router();
 
+const EXPIRATION_WINDOW_SECONDS = 15 * 60;
+
 router.post(
   '/api/orders',
   requireAuth,
@@ -32,31 +34,29 @@ router.post(
       throw new NotFoundError();
     }
 
-    const isReserved = await ticket.isReserved();
-
     // Make sure that this ticket is not already reserved
     // Run query to look at all orders. Find an order with the specific ticket *not* cancelled
-    const existingOrder = await Order.findOne({
-      ticket: ticket,
-      status: {
-        $in: [
-          OrderStatus.Created,
-          OrderStatus.AwaitingPayment,
-          OrderStatus.Complete,
-        ],
-      },
-    });
-    if (existingOrder) {
+    const isReserved = await ticket.isReserved();
+    if (isReserved) {
       throw new BadRequestError('Ticket is already reserved');
     }
 
     // Calculate an expiration date for this order
+    const expiration = new Date();
+    expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS);
 
     // Build the order and save it to the database
+    const order = Order.build({
+      userId: req.currentUser!.id,
+      status: OrderStatus.Created,
+      expiresDate: expiration,
+      ticket,
+    });
+    await order.save();
 
     // Publish an event saying that an order was created
 
-    res.send();
+    res.status(201).send(order);
   }
 );
 
