f47c7cc9e4c690af146152aeec3c15711fdd5b5e
wayson <wayson1225@outlook.com>
2022-03-20 00:48:02 -0700

    implement OCC with mongoose

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -6,7 +6,7 @@ import {
   requireAuth,
   NotAuthorizedError,
 } from '@wayson-ticketing/common';
-import { Ticket } from '../models/tickets';
+import { Ticket } from '../models/ticket';
 import { TicketUpdatedPublisher } from '../events/publishers/ticket-updated-publisher';
 import { natsWrapper } from '../nats-wrapper';
 
