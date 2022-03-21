d9c2fd4e0b84207c7b2f63e2f9e2c4ea81b3645f
wayson <wayson1225@outlook.com>
2022-03-21 01:01:46 -0700

    listener creation

---
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,14 @@
+import {
+  Listener,
+  OrderCreatedEvent,
+  Subjects,
+} from '@wayson-ticketing/common';
+import { Message } from 'node-nats-streaming';
+import { queueGroupName } from './queue-group-name';
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  readonly subject = Subjects.OrderCreated;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: OrderCreatedEvent['data'], msg: Message) {}
+}
