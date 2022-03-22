4e7f50eea46d5ad02e46ae5663e3a2b2ce12af0d
wayson <wayson1225@outlook.com>
2022-03-22 00:32:40 -0700

    initial setup

---
+++ payments/src/test/setup.ts
@@ -0,0 +1,64 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+import request from 'supertest';
+import { app } from '../app';
+import jwt from 'jsonwebtoken';
+
+declare global {
+  var signin: () => string[];
+}
+
+jest.mock('../nats-wrapper');
+
+let mongo: MongoMemoryServer;
+beforeAll(async () => {
+  process.env.JWT_KEY = 'qwer';
+
+  mongo = new MongoMemoryServer();
+  await mongo.start();
+  const mongoUri = mongo.getUri();
+
+  // await mongoose.connect(mongoUri, {
+  //   useNewUrlParser: true,
+  //   useUnifiedTopology: true,
+  // });
+
+  await mongoose.connect(mongoUri);
+});
+
+beforeEach(async () => {
+  jest.clearAllMocks();
+  const collections = await mongoose.connection.db.collections();
+
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
+});
+
+afterAll(async () => {
+  await mongo.stop();
+  await mongoose.connection.close();
+});
+
+global.signin = () => {
+  // Build a JWT payloac. {id,email}
+  const payload = {
+    id: new mongoose.Types.ObjectId().toHexString(),
+    email: 'testing@test.com',
+  };
+  // Create the JWT!
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
+
+  // Build session objet {jwt: MY_JWT}
+  const session = { jwt: token };
+
+  // Turn that session into json
+  const sessionJSON = JSON.stringify(session);
+
+  // Take JSON and encode it as base64
+  const base64 = Buffer.from(sessionJSON).toString('base64');
+
+  // big bug here, the key should according to your own broswer
+  // return a string thats the cookie with the encoded data
+  return [`session=${base64}`];
+};
